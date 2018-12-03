51f360e1bf6b86d859d80545495c505165999b6c
Denis <denis.menendez.a@gmail.com>
2018-12-03 20:43:28 +0100

    Modificacion en path Shared

---
+++ HastenSports/Modules/Shared/APIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants.swift
+//  rssNasa
+//
+//  Created by Pablo Vélez  on 03/122018.
+//  Copyright © 2018 Denis. All rights reserved.
+//
+
+import UIKit
+
+struct APIConstants {
+    
+    static let AuthorizationHeader = "Authorization"
+    static let URLBase = "https://api.myjson.com"
+    static let ImageOfTheDay = "/bins/66851"
+}
---
+++ HastenSports/Modules/Shared/SportsMenListNavigationController.swift
@@ -0,0 +1,12 @@
+//
+//  SportsMenListNavigationController.swift
+//  rssNasa
+//
+//  Created by Pablo Vélez  on 03/12/2018.
+//  Copyright © 2018 Denis. All rights reserved.
+//
+
+import UIKit
+
+class SportsMenListNavigationController: UINavigationController {
+}
